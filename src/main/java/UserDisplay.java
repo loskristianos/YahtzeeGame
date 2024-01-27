46f0e3c071a52d0a49297b964a1938fc9db82000
kris.hardy <kristian.hardy@gmail.com>
2024-01-27 02:30:01 +0000

    userDisplay - added displayScoreCard

--- src/main/java/UserDisplay.java
+++ src/main/java/UserDisplay.java
@@ -8,6 +8,29 @@ public class UserDisplay {
     }
 
     public void displayScoreCard(ScoreCard scoreCard){
-
+        System.out.println("Yahtzee");
+        System.out.println("Upper Section");
+        System.out.println("Aces: " + scoreCard.getAces());
+        System.out.println("Twos: " + scoreCard.getTwos());
+        System.out.println("Threes: " + scoreCard.getThrees());
+        System.out.println("Fours: " + scoreCard.getFours());
+        System.out.println("Fives: " + scoreCard.getFives());
+        System.out.println("Sixes: " + scoreCard.getSixes());
+        System.out.println("Total Score: " + scoreCard.getUpperScore());
+        System.out.println("Bonus: " + scoreCard.getUpperBonus());
+        System.out.println("Total: " + scoreCard.getUpperTotal());
+        System.out.println("============");
+        System.out.println("Lower Section");
+        System.out.println("3 of a Kind: " + scoreCard.getThreeOfAKind());
+        System.out.println("4 of a Kind" + scoreCard.getFourOfAKind());
+        System.out.println("Full House: " + scoreCard.getFullHouse());
+        System.out.println("Small Straight: " + scoreCard.getSmallStraight());
+        System.out.println("Large Straight: " + scoreCard.getLargeStraight());
+        System.out.println("Yahtzee: " + scoreCard.getYahtzee());
+        System.out.println("Chance: " + scoreCard.getChance());
+        System.out.println("Yahtzee Bonus: " + scoreCard.getYahtzeeBonuses());
+        System.out.println("Total of Lower Section: " + scoreCard.getLowerTotal());
+        System.out.println("Total of Upper Section: " + scoreCard.getUpperTotal());
+        System.out.println("Grand Total: " + scoreCard.getGrandTotal());
     }
 }
