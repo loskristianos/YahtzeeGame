6a9bccd59e72ec5f2a86694642722501aba94e72
kris.hardy <kristian.hardy@gmail.com>
2024-03-18 23:33:27 +0000

    Dice.java - make constructor public, remove unnecessary method rollDice

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -2,14 +2,9 @@ public class Dice {
 
     private Integer value;
 
-    private Dice(){}
+    public Dice(){}
 
-    public Dice rollDice(){
-        Dice dice = new Dice();
-        dice.roll();
-        return dice;
-    }
-    private void roll(){
+    public void roll(){
         setValue(1 + (int)(Math.random()*6));
     }
 
