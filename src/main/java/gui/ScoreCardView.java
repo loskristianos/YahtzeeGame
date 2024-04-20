13d0e319110dd71a5f6a741310eb6a17eb106482
kris.hardy <kristian.hardy@gmail.com>
2024-04-20 17:33:46 +0100

    added fields to scoreCard view

--- src/main/java/gui/ScoreCardView.java
+++ src/main/java/gui/ScoreCardView.java
@@ -1,4 +1,29 @@
 package gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class ScoreCardView {
+
+    @FXML private Label aces;
+    @FXML private Label twos;
+    @FXML private Label threes;
+    @FXML private Label fours;
+    @FXML private Label fives;
+    @FXML private Label sixes;
+    @FXML private Label upperScore;
+    @FXML private Label upperBonus;
+    @FXML private Label upperTotal;
+    @FXML private Label threeOfAKind;
+    @FXML private Label fourOfAKind;
+    @FXML private Label fullHouse;
+    @FXML private Label smallStraight;
+    @FXML private Label largeStraight;
+    @FXML private Label yahtzee;
+    @FXML private Label chance;
+    @FXML private Label yahtzeeBonus;
+    @FXML private Label lowerTotal;
+    @FXML private Label grandTotal;
+
+
 }
